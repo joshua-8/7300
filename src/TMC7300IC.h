4f66b5db7ab08e4ca558ce2179efa408e3f5a5ec
joshua-8 <joshua_phelps@brown.edu>
2023-12-03 02:50:38 -0500

    update readme

--- src/TMC7300IC.h
+++ src/TMC7300IC.h
@@ -5,8 +5,6 @@
 
 class TMC7300IC {
 protected:
-    //    0x00, 0x01, 0x02, 0x03, 0x06, 0x10, 0x22, 0x6C, 0x6F, 0x70
-
     // clang-format off
     int32_t registers[10] = {
 //	0	1	2	3	4	5	6	7	8	9
@@ -14,12 +12,11 @@ protected:
     // clang-format on
 
 public:
-    uint32_t writeField(TMCField field, uint32_t fieldValue)
+    void writeField(TMCField field, uint32_t fieldValue)
     {
         uint32_t registerValueToSend = field.setField(fieldValue, registers[field.valueAddress()]);
         writeRegister(field.address(), registerValueToSend);
         registers[field.valueAddress()] = registerValueToSend;
-        return registers[field.valueAddress()];
     }
     uint32_t readField(TMCField field)
     {
