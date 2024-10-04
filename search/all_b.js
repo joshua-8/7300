6ee90331227ff433156ad85dabd4a49b4a9d40d9
joshua-8 <joshua-8@users.noreply.github.com>
2024-10-04 09:05:18 +0000

    deploy: 4c0ec6040bc979a505eae1efd97bb958d4151be1

--- search/all_b.js
+++ search/all_b.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['readfield_0',['readField',['../class_t_m_c7300_i_c.html#aff4f6c61169b64894611b911cf275f5c',1,'TMC7300IC']]],
   ['readme_2emd_1',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
-  ['registers_2',['registers',['../class_t_m_c7300_i_c.html#ae2e8224737e747ec4bcdb6982ae6e2fe',1,'TMC7300IC']]]
+  ['readregister_2',['readRegister',['../class_t_m_c7300_i_c.html#a6591dbe32c6ddd2ce1923641c3793dc6',1,'TMC7300IC']]],
+  ['registers_3',['registers',['../class_t_m_c7300_i_c.html#ae2e8224737e747ec4bcdb6982ae6e2fe',1,'TMC7300IC']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['readfield_0',['readField',['../class_t_m_c7300_i_c.html#aff4f6c61169b64894611b911cf275f5c',1,'TMC7300IC']]]
+  ['readfield_0',['readField',['../class_t_m_c7300_i_c.html#aff4f6c61169b64894611b911cf275f5c',1,'TMC7300IC']]],
+  ['readregister_1',['readRegister',['../class_t_m_c7300_i_c.html#a6591dbe32c6ddd2ce1923641c3793dc6',1,'TMC7300IC']]]
 ];
