0d1563fa038dc73430dbab8819db06ab7924a07b
joshua-8 <joshua-8@users.noreply.github.com>
2024-10-06 06:30:19 +0000

    deploy: 6bce21b0453e55b9683653d290175a443edaaba4

--- search/all_2.js
+++ search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin_0',['begin',['../class_t_m_c7300_i_c.html#a2af07f00fe77a951e2eeb7561d023ad9',1,'TMC7300IC']]]
+  ['begin_0',['begin',['../class_t_m_c7300_i_c.html#a309b2143bcdb2e70f32ea71fb807b63b',1,'TMC7300IC']]]
 ];
